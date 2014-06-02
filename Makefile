3368223c75068bea480a1a820315ffb8b062124b
john stone <john@tuxcode.org>
2014-06-02 20:47:02 +0200

    errorhandling has to be rethought.... g++ buggy.. clang works

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
 
 sqltest: Sqlite3.H sqltest.cc
-	g++-4.9 -std=c++11 sqltest.cc -Wall -ggdb -lsqlite3 -o $@ 
+	clang++ -std=c++11 sqltest.cc -Wall -ggdb -lsqlite3 -o $@ 
 
--- sqltest.cc
+++ sqltest.cc
@@ -11,12 +11,11 @@ int main(int argc, char *argv[])
 
 	SQL::Con db("test.db");
 
-	auto a = db.bindnquery<string,string,string,string>("SELECT 1, 2,3,4  ;");
-	
+	auto a = db.bindnquery< double , string>("SELECT * from a  ;");
+	a.push_back(db.bindnquery<double,string>("Select 3.33333333, 3.3;")[0]);
 	std::cout << "erg :" <<std::endl;
 	for (auto x:a){
 		std::cout << std::get<0>(x) << " " << std::get<1>(x) <<" ";
-		std::cout << std::get<2>(x) << " " << std::get<3>(x) <<" ";
 	std::cout << std::endl;
 	}
 	std::cout << std::endl;
